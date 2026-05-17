a0ea3c4d947be3638f5777dd575fedba63d45855
Trong-Phan <trongvanphan@outlook.com>
2026-05-17 17:50:32 +0700

    clear all to empty for main branch

--- frontend/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@/components/SignIn";
-
-export default function HomePage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-900">
-      <SignIn />
-    </div>
-  );
-}
--- frontend/app/tasks/page.tsx
+++
@@ -1,9 +0,0 @@
-import { TasksContainer } from "@/components/TasksContainer";
-
-interface TasksPageProps {
-  searchParams: { completed?: string };
-}
-
-export default function TasksPage({ searchParams }: TasksPageProps) {
-  return <TasksContainer completed={searchParams.completed} />;
-}
